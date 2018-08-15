3fc6ff4c8fb0fa48b8457383f09e95304cd1cb36
Nick Coultas <nncoultas@yahoo.com>
2018-08-15 16:57:00 -0700

    started About Page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home/Home';
 import Nav from './components/Nav/Nav';
+import About from './components/About/About';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Nav />
-        <Home />
+        <Route path="/Home" component={Home} exact />
+        <Route path="/About" component={About} />
       </div>
     );
   }
