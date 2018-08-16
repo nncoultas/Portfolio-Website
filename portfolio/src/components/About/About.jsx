82f49229b087cb58608d2fa5b297d1fc6f495fc3
Nick Coultas <nncoultas@yahoo.com>
2018-08-16 14:06:38 -0700

    Added avatar to about and styled

--- portfolio/src/components/About/About.jsx
+++ portfolio/src/components/About/About.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
 import './About.css';
+import HeadShot from './image1.jpeg';
+import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+  avatar: {
+    width: 217,
+    height: 287,
+    borderRadius: 32,
+    position: 'absolute',
+    marginLeft: 996
+  }
+};
 
 class About extends React.Component {
   render() {
+    const { classes } = this.props;
     return (
       <div>
         <div className="topPage">
           <Typography variant="display3" color="secondary">
             <div className="aboutMe">About Me</div>
           </Typography>
-          <Typography variant="headline" color="primary" className="jobTitle">
+          <Typography variant="headline" color="primary">
             <div className="jobTitle">Software Engineer from Reno, Nevada</div>
           </Typography>
           <Typography color="primary">
@@ -20,10 +34,11 @@ class About extends React.Component {
               software side of things and creating web applications.
             </div>
           </Typography>
+          <Avatar src={HeadShot} className={classes.avatar} />
         </div>
       </div>
     );
   }
 }
 
-export default About;
+export default withStyles(styles)(About);
