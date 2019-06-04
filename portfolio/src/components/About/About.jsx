d4efeca3588eff119ba5f450dff3a4cfc25885cd
Nick Coultas <nncoultas@yahoo.com>
2019-06-03 20:46:31 -0700

    added more styling and put jobs into cards with a list for discription

--- portfolio/src/components/About/About.jsx
+++ portfolio/src/components/About/About.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import CardContent from '@material-ui/core/CardContent';
+import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 
@@ -11,6 +13,29 @@ const styles = theme => ({
     backgroundColor: '#f5f8fa',
     borderBottom: '1px solid rgba(0,0,0,.125)',
     borderTop: '1px solid rgba(0,0,0,.125)'
+  },
+  bottomPageStyle: {
+    display: 'flex',
+    marginTop: '2%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    width: '30%',
+    flexDirection: 'column'
+  },
+  companyStyle: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    flexGrow: 1,
+    paddingTop: '4%',
+    paddingBottom: '4%',
+    borderBottom: '1px solid rgba(0,0,0,.125)'
+  },
+  cardContentStyle: {
+    paddingTop: '5%'
+  },
+  cardFooterStyle: {
+    paddingTop: '6%',
+    fontSize: '87%'
   }
 });
 class About extends React.Component {
@@ -25,44 +50,73 @@ class About extends React.Component {
             color="secondary"
           >
             Experience
-            <Typography>
+            <Typography variant="headline">
               Want to check out my recent projects I've been working on?
               <br />
               <a href="http://github.com/nncoultas">Check out my github.</a>
             </Typography>
           </Typography>
-          <Typography variant="headline" color="primary">
-            Software Engineer
-          </Typography>
-          <Typography variant="title" color="primary">
-            Lambda School | February 2018 - August 2018
-          </Typography>
-          <Typography color="primary">
-            6+ months Full-Time CS Fundamental/software development program
-          </Typography>
-          <Typography color="primary">
-            Gained experience working with clients and understanding client
-            demands by <br /> collaborating on full lifecycle projects.{' '}
-          </Typography>
-          <Typography color="primary">
-            Completed all curriculum coursework including; <br /> React, react
-            native, redux, node, express, mongoDB, mocha, chai, python, django.{' '}
-            <br />
-            Hands on experience with client and server testing, paired
-            programming, and CS fundamentals.
-          </Typography>
-          <Typography variant="headline" color="primary">
-            Sales Asscoiate
-          </Typography>
-          <Typography variant="title" color="primary">
-            Best Buy | November 2016 - Present
-          </Typography>
-          <Typography color="primary">
-            Home Theater/Computer sales associate with a team of 5+ people
-          </Typography>
-          <Typography color="primary">
-            Required to hit a quota of sales each month both personal and
-            section wide.
+          <Typography className={classes.bottomPageStyle}>
+            <Card>
+              <CardContent>
+                <Typography
+                  className={classes.companyStyle}
+                  variant="h1"
+                  component="h2"
+                  color="primary"
+                >
+                  Lambda School
+                  <Typography>Software Engineer</Typography>
+                </Typography>
+                <Typography
+                  className={classes.cardContentStyle}
+                  color="primary"
+                >
+                  <ul>
+                    <li>
+                      6+ months Full-Time CS Fundamental/software development
+                      program
+                    </li>
+                    <li>
+                      Gained experience working with clients and understanding
+                      client demands by <br /> collaborating on full lifecycle
+                      projects.{' '}
+                    </li>
+                    <li>
+                      {' '}
+                      Completed all curriculum coursework including; <br />{' '}
+                      React, react native, redux, node, express, mongoDB, mocha,
+                      chai, python, django. <br />
+                    </li>
+                    <li>
+                      {' '}
+                      Hands on experience with client and server testing, paired
+                      programming, and CS fundamentals.
+                    </li>
+                  </ul>
+                </Typography>
+                <Typography
+                  className={classes.cardFooterStyle}
+                  variant="subtitle1"
+                  color="textSecondary"
+                >
+                  February 2018 - August 2018
+                </Typography>
+              </CardContent>
+            </Card>
+            <Typography variant="headline" color="primary">
+              Sales Asscoiate
+            </Typography>
+            <Typography variant="title" color="primary">
+              Best Buy | November 2016 - Present
+            </Typography>
+            <Typography color="primary">
+              Home Theater/Computer sales associate with a team of 5+ people
+            </Typography>
+            <Typography color="primary">
+              Required to hit a quota of sales each month both personal and
+              section wide.
+            </Typography>
           </Typography>
         </Typography>
       </div>
