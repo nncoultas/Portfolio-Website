f33d6a71eee2076503a9fc4ba98dedfe3cc8851e
Nick Coultas <nncoultas@yahoo.com>
2019-06-05 21:26:29 -0700

    added logos for skills in certain languages

--- portfolio/src/components/Skills/Skills.jsx
+++ portfolio/src/components/Skills/Skills.jsx
@@ -1,39 +1,86 @@
 import React from 'react';
-import Chip from '@material-ui/core/Chip';
+import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
+import CardContent from '@material-ui/core/CardContent';
+import Card from '@material-ui/core/Card';
 import { withStyles } from '@material-ui/core/styles';
 
-const styles = () => {};
+import JavascriptIcon from './javascript-logo.png';
+import HtmlIcon from './HTML-logo.png';
+import CIcon from './C-Logo.png';
+import PythonIcon from './Python-logo.png';
+
+const styles = () => ({
+  topPageStyle: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginTop: '2%',
+    backgroundColor: '#f5f8fa',
+    borderBottom: '1px solid rgba(0,0,0,.125)',
+    borderTop: '1px solid rgba(0,0,0,.125)'
+  },
+  cardTitleStyle: {},
+  CardContainerStyle: {
+    display: 'flex',
+    flexGrow: '1',
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  avatarStyle: {
+    width: '7%',
+    height: '0%'
+  }
+});
 
 class Skills extends React.Component {
-  copyEmail = () => {
-    let textField = document.createElement('textarea');
-    textField.innerText = 'nncoultas@yahoo.com';
-    document.body.appendChild(textField);
-    textField.select();
-    document.execCommand('copy');
-    textField.remove();
-    document.getElementById('email').title =
-      'My email address has been copied to your clipboard!';
-  };
   render() {
     const { classes } = this.props;
     return (
       <div>
-        <Typography className="bottomPage">
-          <Typography variant="display3" color="secondary">
+        <Typography>
+          <Typography
+            variant="display3"
+            color="secondary"
+            className={classes.topPageStyle}
+          >
             Skills
           </Typography>
           <Typography>
-            <Typography variant="headline" color="Primary">
-              Languages
-            </Typography>
-            <Typography variant="headline" color="Primary">
-              Front-End
-            </Typography>
-            <Typography variant="headline" color="Primary">
-              Back-End
-            </Typography>
+            <Card>
+              <CardContent>
+                <Typography
+                  className={classes.cardTitleStyle}
+                  variant="headline"
+                  color="Primary"
+                >
+                  Languages
+                </Typography>
+                <div className={classes.CardContainerStyle}>
+                  <Avatar
+                    src={JavascriptIcon}
+                    className={classes.avatarStyle}
+                  />
+                  <Avatar src={HtmlIcon} className={classes.avatarStyle} />
+                  <Avatar src={CIcon} className={classes.avatarStyle} />
+                  <Avatar src={PythonIcon} className={classes.avatarStyle} />
+                </div>
+              </CardContent>
+            </Card>
+            <Card>
+              <CardContent>
+                <Typography variant="headline" color="Primary">
+                  Front-End
+                </Typography>
+              </CardContent>
+            </Card>
+            <Card>
+              <CardContent>
+                <Typography variant="headline" color="Primary">
+                  Back-End
+                </Typography>
+              </CardContent>
+            </Card>
           </Typography>
         </Typography>
       </div>
