3bd3f64a11ef44f465b897f7422312e8a6cdf79f
Nick Coultas <nncoultas@yahoo.com>
2019-06-09 15:48:48 -0700

    added title to blog page

--- portfolio/src/components/Blog/Blog.jsx
+++ portfolio/src/components/Blog/Blog.jsx
@@ -10,6 +10,15 @@ import Macbook from './clement-h-544786-unsplash.jpg';
 import './Blog.css';
 
 const styles = {
+  topPageStyle: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginTop: '2%',
+    backgroundColor: '#f5f8fa',
+    borderBottom: '1px solid rgba(0,0,0,.125)',
+    borderTop: '1px solid rgba(0,0,0,.125)'
+  },
   card: {
     maxWidth: 462,
     marginTop: 68
@@ -24,41 +33,51 @@ class Blog extends React.Component {
   render() {
     const { classes } = this.props;
     return (
-      <div className="blogView">
-        <Card className={classes.card}>
-          <CardMedia
-            className={classes.media}
-            image={Macbook}
-            title="macbook"
-          />
-          <CardContent>
-            <Typography
-              gutterBottom
-              variant="headline"
-              component="h2"
-              color="secondary"
-            >
-              5 Ways A Coding Academy Teaches You How To Be A Programmer
-            </Typography>
-            <Typography component="p" color="primary">
-              Background My name is Nick Coultas and I currently reside in Reno,
-              Nevada. There is a very small community here as far as
-              Programmers/Developers go. I soon realized this when I started....
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <a
-              href={
-                'https://medium.com/@nncoultas/5-ways-a-coding-academy-teaches-you-how-to-be-a-programmer-41e13b4e33c8'
-              }
-              style={{ textDecoration: 'none' }}
-            >
-              <Button size="small" color="secondary">
-                Read More
-              </Button>
-            </a>
-          </CardActions>
-        </Card>
+      <div>
+        <Typography
+          className={classes.topPageStyle}
+          variant="display3"
+          color="secondary"
+        >
+          Blog
+        </Typography>
+        <div className="blogView">
+          <Card className={classes.card}>
+            <CardMedia
+              className={classes.media}
+              image={Macbook}
+              title="macbook"
+            />
+            <CardContent>
+              <Typography
+                gutterBottom
+                variant="headline"
+                component="h2"
+                color="secondary"
+              >
+                5 Ways A Coding Academy Teaches You How To Be A Programmer
+              </Typography>
+              <Typography component="p" color="primary">
+                Background My name is Nick Coultas and I currently reside in
+                Reno, Nevada. There is a very small community here as far as
+                Programmers/Developers go. I soon realized this when I
+                started....
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <a
+                href={
+                  'https://medium.com/@nncoultas/5-ways-a-coding-academy-teaches-you-how-to-be-a-programmer-41e13b4e33c8'
+                }
+                style={{ textDecoration: 'none' }}
+              >
+                <Button size="small" color="secondary">
+                  Read More
+                </Button>
+              </a>
+            </CardActions>
+          </Card>
+        </div>
       </div>
     );
   }
